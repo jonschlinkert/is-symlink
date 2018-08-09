6f33ffe918433db38331a341e128c4f53485739a
jonschlinkert <jon.schlinkert@sellside.com>
2018-08-09 03:50:41 -0400

    support promises

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 /*!
  * is-symlink <https://github.com/jonschlinkert/is-symlink>
  *
- * Copyright (c) 2016, Jon Schlinkert.
+ * Copyright (c) 2016-present, Jon Schlinkert.
  * Licensed under the MIT License.
  */
 
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
 
-module.exports = function(filepath, cb) {
+const isSymlink = (filepath, cb) => {
   if (typeof cb !== 'function') {
-    throw new TypeError('expected a callback function');
+    return util.promisify(isSymlink)(filepath);
   }
+
   if (typeof filepath !== 'string') {
     cb(new TypeError('expected filepath to be a string'));
     return;
   }
 
-  fs.lstat(path.resolve(filepath), function(err, stats) {
-    if (err) return cb(err);
+  fs.lstat(path.resolve(filepath), (err, stats) => {
+    if (err) {
+      cb(err);
+      return;
+    }
     cb(null, stats.isSymbolicLink());
   });
 };
 
-module.exports.sync = function(filepath) {
+isSymlink.sync = filepath => {
   if (typeof filepath !== 'string') {
     throw new TypeError('expected filepath to be a string');
   }
   try {
-    var stats = fs.lstatSync(path.resolve(filepath));
+    let stats = fs.lstatSync(path.resolve(filepath));
     return stats.isSymbolicLink();
   } catch (err) {
-    if (err.code === 'ENOENT') {
-      return false;
-    }
+    if (err.code === 'ENOENT') return false;
     throw err;
   }
 };
+
+module.exports = isSymlink;
--- test.js
+++ test.js
@@ -1,66 +1,58 @@
 'use strict';
 
 require('mocha');
-var fs = require('fs');
-var assert = require('assert');
-var isSymlink = require('./');
+const fs = require('fs');
+const assert = require('assert');
+const isSymlink = require('./');
 
-describe('isSymlink', function() {
-  it('should export a function', function() {
+describe('isSymlink', () => {
+  it('should export a function', () => {
     assert.equal(typeof isSymlink, 'function');
   });
 
-  describe('errors', function() {
-    it('should throw an error when callback is not passed', function(cb) {
-      try {
-        isSymlink();
-        cb(new Error('expected an error'));
-      } catch (err) {
-        assert(err);
-        assert.equal(err.message, 'expected a callback function');
-        cb();
-      }
+  describe('errors', () => {
+    it('should throw an error when callback is not passed', () => {
+      return isSymlink()
+        .then(() => {
+          throw new Error('expected an error');
+        })
+        .catch(err => {
+          assert(/expected filepath/.test(err.message));
+        })
     });
 
-    it('should throw an error when filepath is not a string', function(cb) {
-      isSymlink(null, function(err, symlink) {
+    it('should throw an error when filepath is not a string', cb => {
+      isSymlink(null, err => {
         assert(err);
         assert.equal(err.message, 'expected filepath to be a string');
         cb();
       });
     });
 
-    it('should throw an error when filepath is not a string', function(cb) {
-      try {
-        isSymlink.sync(null, function() {});
-        cb(new Error('expected an error'));
-      } catch (err) {
-        assert(err);
-        assert.equal(err.message, 'expected filepath to be a string');
-        cb();
-      }
+    it('should throw an error when filepath is not a string', () => {
+      assert.throws(() => isSymlink.sync(null, () => {}), /expected filepath/);
     });
   });
 
-  describe('async', function() {
-    beforeEach(function(cb) {
+  describe('async', () => {
+    beforeEach(cb => {
       fs.symlink('index.js', 'foo.js', cb);
     });
 
-    afterEach(function(cb) {
+    afterEach(cb => {
       fs.unlink('foo.js', cb);
     });
 
-    it('should return false if a path is not a symlink', function(cb) {
-      isSymlink('index.js', function(err, is) {
+    it('should return false if a path is not a symlink', cb => {
+      isSymlink('index.js', (err, is) => {
         if (err) return cb(err);
         assert(!is);
         cb();
       });
     });
 
-    it('should return true if a path is a symlink', function(cb) {
-      isSymlink('foo.js', function(err, is) {
+    it('should return true if a path is a symlink', cb => {
+      isSymlink('foo.js', (err, is) => {
         if (err) return cb(err);
         assert(is);
         cb();
@@ -68,21 +60,21 @@ describe('isSymlink', function() {
     });
   });
 
-  describe('sync', function() {
-    beforeEach(function(cb) {
+  describe('sync', () => {
+    beforeEach(cb => {
       fs.symlink('index.js', 'foo.js', cb);
     });
 
-    afterEach(function(cb) {
+    afterEach(cb => {
       fs.unlink('foo.js', cb);
     });
 
-    it('should return false if a path is not a symlink', function(cb) {
+    it('should return false if a path is not a symlink', cb => {
       assert(!isSymlink.sync('index.js'));
       cb();
     });
 
-    it('should return true if a path is a symlink', function(cb) {
+    it('should return true if a path is a symlink', cb => {
       assert(isSymlink.sync('foo.js'));
       cb();
     });
